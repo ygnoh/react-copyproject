0ce6d548cbdbf1cb44d0e9b862b4ac0f7bbff8e2
yonggoonoh <nohyg90@gmail.com>
2017-11-23 21:40:44 +0900

    setting: dev server setting

--- server/main.js
+++ server/main.js
@@ -1,3 +1,8 @@
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack'
+
+const devPort = 4000;
+
 import express from 'express';
 import path from 'path';
 
@@ -13,3 +18,15 @@ app.get('/hello', (req, res) => {
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
+
+if (process.env.NODE_ENV === 'development') {
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
