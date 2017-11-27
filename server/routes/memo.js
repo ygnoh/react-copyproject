abf84721cdbac3c8767170aba3191b74b13f6a06
yonggoonoh <nohyg90@gmail.com>
2017-11-27 23:25:38 +0900

    feat: implement modify memo

--- server/routes/memo.js
+++ server/routes/memo.js
@@ -106,4 +106,79 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+/*
+    MODIFY MEMO: PUT /api/memo/:id
+    BODY SAMPLE: { contents: "sample "}
+    ERROR CODES
+        1: INVALID ID,
+        2: EMPTY CONTENTS
+        3: NOT LOGGED IN
+        4: NO RESOURCE
+        5: PERMISSION FAILURE
+*/
+router.put('/:id', (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({
+            error: 'INVALID ID',
+            code: 1
+        });
+    }
+
+    if (typeof req.body.contents !== 'string') {
+        return res.status(400).json({
+            error: 'EMPTY CONTENTS',
+            code: 2
+        });
+    }
+
+    if (req.body.contents === '') {
+        return res.status(400).json({
+            error: 'EMPTY CONTENTS',
+            code: 2
+        });
+    }
+
+    if (typeof req.session.loginInfo === 'undefined') {
+        return res.status(403).json({
+            error: 'NOT LOGGED IN',
+            code: 3
+        });
+    }
+
+    Memo.findById(req.params.id, (err, memo) => {
+        if (err) {
+            throw err;
+        }
+
+        if (!memo) {
+            return res.status(404).json({
+                error: 'NO RESOURCE',
+                code: 4
+            });
+        }
+
+        if (memo.writer !== req.session.loginInfo.username) {
+            return res.status(403).json({
+                error: 'PERMISSON FAILURE',
+                code: 5
+            });
+        }
+
+        memo.contents = req.body.contents;
+        memo.date.edited = new Date();
+        memo.is_edited = true;
+
+        memo.save((err, memo) => {
+            if (err) {
+                throw err;
+            }
+            
+            return res.json({
+                success: true,
+                memo
+            });
+        });
+    });
+});
+
 export default router
