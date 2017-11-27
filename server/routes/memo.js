a8e6342091ca18161669ef9c6b0985820f747f40
yonggoonoh <nohyg90@gmail.com>
2017-11-27 23:05:59 +0900

    feat: create basic memo router

---
+++ server/routes/memo.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Memo from '../models/memo';
+import mongoose from 'mongoose';
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+export default router
