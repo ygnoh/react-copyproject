b742528b3d5ec95920d2a3be72762a35c17b6ee7
yonggoonoh <nohyg90@gmail.com>
2017-11-27 23:11:44 +0900

    feat: implement write memo

--- server/routes/memo.js
+++ server/routes/memo.js
@@ -1,11 +1,44 @@
 import express from 'express';
 import Memo from '../models/memo';
 import mongoose from 'mongoose';
+import { watch } from 'fs';
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
+    if (typeof req.session.loginInfo === 'undefined') {
+        return res.status(403).json({
+            error: 'NOT LOGGED IN',
+            code: 1
+        });
+    }
 
+    if (typeof req.body.contents !== 'string') {
+        return res.status(400).json({
+            error: 'EMPTY CONTENTS',
+            code: 2
+        });
+    }
+
+    if (req.body.contents === '') {
+        return res.status(400).json({
+            error: 'EMPTY CONTENTS',
+            code: 2
+        });
+    }
+
+    let memo = new Memo({
+        writer: req.session.loginInfo.username,
+        contents: req.body.contents
+    });
+
+    memo.save(err => {
+        if (err) {
+            throw err;
+        }
+
+        return res.json({ success: true });
+    });
 });
 
 router.put('/:id', (req, res) => {
