1f3a0806a3059c63f6527059b146c6732f25fe56
yonggoonoh <nohyg90@gmail.com>
2017-11-28 00:40:19 +0900

    feat: fill the header contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
-        )
+            <nav>
+                <div className="nav-wrapper blue darken-1">
+                    <a className="brand-logo center">MEMOPAD</a>
+                    <ul>
+                        <li><a><i className="material-icons">search</i></a></li>
+                    </ul>
+                    <div className="right">
+                        <ul>
+                            <li>
+                                <a><i className="material-icons">vpn_key</i></a>
+                            </li>
+                            <li>
+                                <a><i className="material-icons">lock_open</i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
     }
-};
+}
 
 export default Header
