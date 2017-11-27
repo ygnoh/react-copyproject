2ff5682dee5a000a5afecc7dc681bf6a2e14d880
yonggoonoh <nohyg90@gmail.com>
2017-11-27 22:49:57 +0900

    refact: convert spacing indentation to tab indentation

--- server/models/account.js
+++ server/models/account.js
@@ -4,19 +4,19 @@ import bcrypt from 'bcryptjs';
 const Schema = mongoose.Schema;
 
 const Account = new Schema({
-    username: String,
-    password: String,
-    created: { type: Date, default: Date.now }
+	username: String,
+	password: String,
+	created: { type: Date, default: Date.now }
 });
 
 // Generate hash
 Account.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, 8);
+	return bcrypt.hashSync(password, 8);
 };
 
 // compare the password
 Account.methods.validateHash = function(password) {
-    return bcrypt.compareSync(password, this.password);
+	return bcrypt.compareSync(password, this.password);
 };
 
 export default mongoose.model('account', Account)
--- server/routes/account.js
+++ server/routes/account.js
@@ -4,98 +4,98 @@ import Account from '../models/account';
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
-    const usernameRegex = /^[a-z0-9]+$/;
+	const usernameRegex = /^[a-z0-9]+$/;
 
-    // check if the username consists of character and number
-    if (!usernameRegex.test(req.body.username)) {
-        return res.status(400).json({
-            error: 'BAD USERNAME',
-            code: 1
-        });
-    }
+	// check if the username consists of character and number
+	if (!usernameRegex.test(req.body.username)) {
+		return res.status(400).json({
+			error: 'BAD USERNAME',
+			code: 1
+		});
+	}
 
-    // check password length
-    if (req.body.password.length < 4 || typeof req.body.password !== 'string') {
-        return res.status(400).json({
-            error: 'BAD PASSWORD',
-            code: 2
-        });
-    }
+	// check password length
+	if (req.body.password.length < 4 || typeof req.body.password !== 'string') {
+		return res.status(400).json({
+			error: 'BAD PASSWORD',
+			code: 2
+		});
+	}
 
-    // check user existance
-    Account.findOne({ username: req.body.username }, (err, exsists) => {
-        if (err) {
-            throw err;
-        }
+	// check user existance
+	Account.findOne({ username: req.body.username }, (err, exsists) => {
+		if (err) {
+			throw err;
+		}
 
-        if (exists) {
-            return res.status(409).json({
-                error: 'USERNAME EXISTS',
-                code: 3
-            });
-        }
+		if (exists) {
+			return res.status(409).json({
+				error: 'USERNAME EXISTS',
+				code: 3
+			});
+		}
 
-        // create and save the user info
-        let account = new Account({
-            username: req.body.username,
-            password: req.body.password
-        });
+		// create and save the user info
+		let account = new Account({
+			username: req.body.username,
+			password: req.body.password
+		});
 
-        account.password = account.generateHash(account.password);
+		account.password = account.generateHash(account.password);
 
-        account.save(err => {
-            if (err) {
-                throw err;
-            }
-            return res.json({ success: true });
-        });
-    });
+		account.save(err => {
+			if (err) {
+				throw err;
+			}
+			return res.json({ success: true });
+		});
+	});
 });
 
 router.post('/signin', (req, res) => {
-    if (typeof req.body.password !== 'string') {
-        return res.status(401).json({
-            error: 'LOGIN FAILED',
-            code: 1
-        });
-    }
+	if (typeof req.body.password !== 'string') {
+		return res.status(401).json({
+			error: 'LOGIN FAILED',
+			code: 1
+		});
+	}
 
-    Account.findOne({ username: req.body.username }, (err, account) => {
-        if (err) {
-            throw err;
-        }
+	Account.findOne({ username: req.body.username }, (err, account) => {
+		if (err) {
+			throw err;
+		}
 
-        if (!account) {
-            return res.status(401).json({
-                error: 'LOGIN FAILED',
-                code: 1
-            });
-        }
+		if (!account) {
+			return res.status(401).json({
+				error: 'LOGIN FAILED',
+				code: 1
+			});
+		}
 
-        if (!account.validateHash(req.body.password)) {
-            return res.status(401).json({
-                error: 'LOGIN FAILED',
-                code: 1
-            });
-        }
+		if (!account.validateHash(req.body.password)) {
+			return res.status(401).json({
+				error: 'LOGIN FAILED',
+				code: 1
+			});
+		}
 
-        // alter session
-        let session = req.session;
-        session.loginInfo = {
-            _id: account._id,
-            username: account.username
-        };
+		// alter session
+		let session = req.session;
+		session.loginInfo = {
+			_id: account._id,
+			username: account.username
+		};
 
-        return res.json({ success: true });
-    });
+		return res.json({ success: true });
+	});
 });
 
 router.get('/getinfo', (req, res) => {
-    res.json({ info: null });
+	res.json({ info: null });
 });
 
 router.post('/logout', (req, res) => {
-    return res.json({ success: true });
+	return res.json({ success: true });
 });
 
 export default router
