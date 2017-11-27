854ca0bc2fb17ccae66ceed02df7a4d6073d13ef
yonggoonoh <nohyg90@gmail.com>
2017-11-27 22:30:57 +0900

    feat: 비밀번호 해시화

--- server/models/account.js
+++ server/models/account.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const Schema = mongoose.Schema;
 
@@ -7,5 +8,15 @@ const Account = new Schema({
     password: String,
     created: { type: Date, default: Date.now }
 });
+
+// Generate hash
+Account.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, 8);
+};
+
+// compare the password
+Account.methods.validateHash = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 export default mongoose.model('account', Account)
