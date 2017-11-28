fa39997872ff5962b1b8aec36eb335c95a13a2f8
yonggoonoh <nohyg90@gmail.com>
2017-11-29 01:42:22 +0900

    feat: basic routing setting

--- server/main.js
+++ server/main.js
@@ -23,6 +23,10 @@ app.use(function(err, req, res, next) {
     res.status(500).send('Something broke!');
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './../public/index.html'));
+});
+
 app.get('/hello', (req, res) => {
     return res.send('Hello');
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from 'containers';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { App, Home, Login, Register } from 'containers';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/home" component={Home}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/register" component={Register}/>
+        </div>
+    </BrowserRouter>, rootElement
+);
