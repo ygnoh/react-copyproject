f7e8db27163353d7a7129cf6bbc7b0cafa02149d
yonggoonoh <nohyg90@gmail.com>
2017-11-29 23:59:13 +0900

    feat: header rendering 시점 변경

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Header } from 'components';
 
 class App extends Component {
     render(){
         return (
             <div>
-                <Header />
                 { this.props.children }
             </div>
         );
