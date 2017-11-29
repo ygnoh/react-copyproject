33e9cdf87993a97a47df40e7e7b68dea71058d58
yonggoonoh <nohyg90@gmail.com>
2017-11-30 01:21:49 +0900

    feat: login container에 handleLogin 구현

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
-import { Authentication } from 'components';
 import { connect } from 'react-redux';
+
+import { Authentication } from 'components';
 import { loginRequest } from 'actions/authentication';
+import history from '../history';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    handleLogin(id, pw) {
+        return this.props.loginRequest(id, pw).then( () => {
+            if (this.props.status === "SUCCESS") {
+                let loginData = {
+                    isLoggedIn: true,
+                    username: id
+                };
+
+                document.cookie = 'key=' + btoa(JSON.stringify(loginData));
+
+                Materialize.toast('Welcome, ' + id + '!', 2000);
+                history.push('/');
+
+                return true;
+            } else {
+                let $toastContent = $('<span style="color: #FFB4BA">Incorrect username or password</span>');
+                Materialize.toast($toastContent, 2000);
+
+                return false;
+            }
+        });
+    }
+
     render() {
         return (
             <div>
-                <Authentication mode={true}/>
+                <Authentication mode={true} onLogin={this.handleLogin} />
             </div>
         )
     }
