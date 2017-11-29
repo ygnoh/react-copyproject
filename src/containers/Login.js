47972932be73c96af044043f6e47d9fbb9bcebfa
yonggoonoh <nohyg90@gmail.com>
2017-11-30 01:11:54 +0900

    feat: login container 구현

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Authentication } from 'components';
+import { connect } from 'react-redux';
+import { loginRequest } from 'actions/authentication';
 
-class  Login extends Component {
+class Login extends Component {
     render() {
         return (
             <div>
@@ -11,4 +13,18 @@ class  Login extends Component {
     }
 }
 
-export default Login
+const mapStateToProps = (state) => {
+    return {
+        status: state.authentication.login.status
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loginRequest: (id, pw) => {
+            return dispatch(loginRequest(id, pw));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
