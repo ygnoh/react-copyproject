8b8139fbd465f0aa6cd2db63e5e8becfd65cf1c4
yonggoonoh <nohyg90@gmail.com>
2017-11-30 00:08:29 +0900

    feat: login과 register 페이지에서는 header를 보여주지 않음

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Header } from 'components';
 
 class App extends Component {
     render(){
+        const re = /(login|register)/;
+        const isAuth = re.test(this.props.location.pathname);
+
         return (
             <div>
+                {isAuth ? undefined : <Header />}
                 { this.props.children }
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import { Header } from 'components';
 import { App, Home, Login, Register } from 'containers';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
     <BrowserRouter>
         <div>
-            <Header />
             <Route exact path="/" component={App}/>
             <Route path="/home" component={Home}/>
             <Route path="/login" component={Login}/>
