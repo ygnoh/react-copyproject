65925fb64bcb751b4526c62ffd01b7ceea701dcf
yonggoonoh <nohyg90@gmail.com>
2017-11-30 01:03:13 +0900

    feat: thunk를 이용하여 loginRequest 구현

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,11 +1,23 @@
+import axios from 'axios';
 import {
     AUTH_LOGIN,
     AUTH_LOGIN_SUCCESS,
     AUTH_LOGIN_FAILURE
 } from './ActionTypes';
 
+/* THIS USES THUNK */
 export function loginRequest(username, password) {
+    return (dispatch) => {
+        dispatch(login());
 
+        return axios.post('/api/account/signin', { username, password })
+                    .then( response => {
+                        dispatch(loginSuccess(username));
+                    })
+                    .catch( error => {
+                        dispatch(loginFailure());
+                    });
+    }
 }
 
 export function login() {
