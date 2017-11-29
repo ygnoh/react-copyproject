51c1598bdea76265e747a4169a1dc662a507f0b0
yonggoonoh <nohyg90@gmail.com>
2017-11-30 00:20:09 +0900

    feat: create basic auth component

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Authentication extends Component {
     render () {
+        const loginView = (
+            <div>loginView</div>
+        );
+
+        const registerView = (
+            <div>registerView</div>
+        );
+
         return (
-            <div>
-                Auth
+            <div className="container auth">
+                <Link className="logo" to="/">MEMOPAD</Link>
+                <div className="card">
+                    <div className="header blue white-text center">
+                        <div className="card-content">{this.props.mode ? "LOGIN" : "REGISTER"}</div>
+                    </div>
+                    {this.props.mode ? loginView : registerView}
+                </div>
             </div>
         );
     }
 }
+
+Authentication.propTypes = {
+    mode: React.PropTypes.bool,
+    onLogin: React.PropTypes.func,
+    onRegister: React.PropTypes.func
+};
+
+Authentication.defaultProps = {
+    mode: true,
+    onLogin: (id, pw) => { console.error("login function not defined"); },
+    onRegister: (id, pw) => { console.error("register function not defined"); }
+};
 
 export default Authentication
