f8f2a3ace99262331329f93103672296a29d2e52
yonggoonoh <nohyg90@gmail.com>
2017-11-30 00:11:42 +0900

    feat: login, register 페이지에서는 auth 페이지 렌더링

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Authentication } from 'components';
 
 class  Login extends Component {
     render() {
         return (
             <div>
-                Login
+                <Authentication />
             </div>
         )
     }
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Authentication } from 'components';
 
 class Register extends Component {
     render () {
         return (
             <div>
-                Register
+                <Authentication />
             </div>
         )
     }
