14ebd1690221c386ea0a5368163426fa20bb1577
yonggoonoh <nohyg90@gmail.com>
2017-11-30 00:53:55 +0900

    feat: redux 적용

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { App, Home, Login, Register } from 'containers';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route exact path="/" component={App}/>
-            <Route path="/home" component={Home}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-        </div>
-    </BrowserRouter>, rootElement
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={App}/>
+                <Route path="/home" component={Home}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+            </div>
+        </BrowserRouter>
+    </Provider>, rootElement
 );
