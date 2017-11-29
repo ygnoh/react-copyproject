399ef0acded7f3636d836528c7b81cf2c6766e6a
yonggoonoh <nohyg90@gmail.com>
2017-11-30 01:08:47 +0900

    feat: 로그인 리듀서 구현

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,6 +1,47 @@
+import * as types from 'actions/ActionTypes';
+import update from 'react-addons-update';
+
+const initialState = {
+    login: {
+        status: 'INIT'
+    },
+    status: {
+        isLoggedIn: false,
+        currentUser: '',
+    }
+};
+
+
 export default function authentication(state, action) {
     if (typeof state === 'undefined') {
-        state = {};
+        state = initialState;
+    }
+
+    switch(action.types) {
+        case types.AUTH_LOGIN:
+            return update(state, {
+                login: {
+                    status: { $set: 'WAITING' }
+                }
+            });
+        case types.AUTH_LOGIN_SUCCESS:
+            return update(state, {
+                login: {
+                    status: { $set: 'SUCCESS'}
+                },
+                status: {
+                    isLoggedIn: { $set: true },
+                    currentUser: { $set: action.username }
+                }
+            });
+        case types.AUTH_LOGIN_FAILURE:
+            return update(state, {
+                login: {
+                    status: { $set: 'FAILURE' }
+                }
+            });
+        default:
+            return state;
     }
 
     return state;
