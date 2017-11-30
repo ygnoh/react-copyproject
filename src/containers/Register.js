56108223c1d98725a5d196d8fd62ab5df6c17a1c
yonggoonoh <nohyg90@gmail.com>
2017-12-01 00:21:16 +0900

    feat: register redux와 연결

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Authentication } from 'components';
+import { connect } from 'react-redux';
+import { registerRequest } from 'actions/authentication';
 
 class Register extends Component {
     render () {
@@ -11,4 +13,19 @@ class Register extends Component {
     }
 }
 
-export default Register
+const mapStateToProps = (state) => {
+    return {
+        status: state.authentication.register.status,
+        errorCode: state.authentication.register.error
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        registerRequest: (id, pw) => {
+            return dispatch(registerRequest(id, pw));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register)
