c31546b1ff489a901f5630ba8593acb663564b49
yonggoonoh <nohyg90@gmail.com>
2017-11-30 23:36:21 +0900

    feat: 회원가입 action 뼈대 설정

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import {
     AUTH_LOGIN,
     AUTH_LOGIN_SUCCESS,
-    AUTH_LOGIN_FAILURE
+    AUTH_LOGIN_FAILURE,
+    AUTH_REGISTER,
+    AUTH_REGISTER_SUCCESS,
+    AUTH_REGISTER_FAILURE
 } from './ActionTypes';
 
 /* THIS USES THUNK */
@@ -36,5 +39,30 @@ export function loginSuccess(username) {
 export function loginFailure() {
     return {
         type: AUTH_LOGIN_FAILURE
+    };
+}
+
+export function registerRequest(username, password) {
+    return (dispatch) => {
+
+    };
+}
+
+export function register() {
+    return {
+        type: AUTH_REGISTER
+    };
+}
+
+export function registerSuccess() {
+    return {
+        type: AUTH_REGISTER_SUCCESS
+    };
+}
+
+export function registerFailure(error) {
+    return {
+        type: AUTH_REGISTER_FAILURE,
+        error
     };
 }
