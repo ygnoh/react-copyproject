ba4a591fe1461e21250c09fa37e1c5dfe513c29f
yonggoonoh <nohyg90@gmail.com>
2017-12-05 22:47:33 +0900

    feat: login 상태인 경우 write 컴포넌트 노출

---
+++ src/components/Write.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Write extends Component {
+    render() {
+        return (
+            <div>
+                Write
+            </div>
+        )
+    }
+}
+
+export default Write
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Write } from 'components';
 
 class Home extends Component {
     render() {
+        const write = (<Write />);
         return (
             <div>
-                Home
+                {this.props.isLoggedIn ? write : undefined}
             </div>
-        )
+        );
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.authentication.status.isLoggedIn
+    };
+};
+
+export default connect(mapStateToProps)(Home)
