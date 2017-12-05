83b705ac6d52aa9f7a2e901886691a4241ff7b4a
yonggoonoh <nohyg90@gmail.com>
2017-12-05 23:22:13 +0900

    feat: 메모 작성시 호출되는 handlePost 구현

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,10 +4,47 @@ import { Write } from 'components';
 import { memoPostRequest } from 'actions/memo';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.handlePost = this.handlePost.bind(this);
+    }
+
+    handlePost(contents) {
+        return this.props.memoPostRequest(contents).then(() => {
+            if (this.props.postStatus.status === 'SUCCESS') {
+                Materialize.toast('Success!', 2000);
+            } else {
+                let $toastContent;
+
+                switch(this.props.postStatus.error) {
+                    case 1:
+                        $toastContent = $('<span style="color: #FFB4BA">You are not logged in</span>');
+                        Materialize.toast($toastContent, 2000);
+                        setTimeout(() => {location.reload(false);}, 2000);
+                        break;
+
+                    case 2:
+                        $toastContent = $('<span style="color: #FFB4BA">Please write something</span>');
+                        Materialize.toast($toastContent, 2000);
+                        break;
+
+                    default:
+                        $toastContent = $('<span style="color: #FFB4BA">Something Broke</span>');
+                        Materialize.toast($toastContent, 2000);
+                        break;
+                }
+            }
+        });
+    }
+
     render() {
+        const write = (
+            <Write onPost={this.handlePost} />
+        );
+
         return (
             <div className="wrapper">
-                {this.props.isLoggedIn ? <Write/> : undefined}
+                {this.props.isLoggedIn ? write : undefined}
             </div>
         );
     }
