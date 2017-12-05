e16e261d7eed04c1cf0c7f417240ed0cae80d525
yonggoonoh <nohyg90@gmail.com>
2017-12-05 22:52:56 +0900

    feat: write component 내부 구현

--- src/components/Write.js
+++ src/components/Write.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Write extends Component {
     render() {
         return (
-            <div>
-                Write
+            <div className="container write">
+                <div className="card">
+                    <div className="card-content">
+                        <textarea className="materialize-textarea" placeholder="Write down your memo"></textarea>
+                    </div>
+                    <div className="card-action">
+                        <a>POST</a>
+                    </div>
+                </div>
             </div>
         )
     }
