b779c1e33bbe4a378233a1e66fc1c0641473c3ab
yonggoonoh <nohyg90@gmail.com>
2017-12-05 23:29:55 +0900

    feat: Memo, MemoList component 생성

---
+++ src/components/Memo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Memo extends Component {
+    render() {
+        return (
+            <div>
+                Memo
+            </div>
+        )
+    }
+}
+
+export default Memo
---
+++ src/components/MemoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MemoList extends Component {
+    render() {
+        return (
+            <div>
+                MemoList
+            </div>
+        )
+    }
+}
+
+export default MemoList
