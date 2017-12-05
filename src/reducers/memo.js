0856143d8cd9b15c6e6a43c56de3b511099f72b8
yonggoonoh <nohyg90@gmail.com>
2017-12-06 00:20:07 +0900

    feat: memo reducer 수정

--- src/reducers/memo.js
+++ src/reducers/memo.js
@@ -5,6 +5,11 @@ const initialState = {
     post: {
         status: 'INIT',
         error: -1
+    },
+    list: {
+        status: 'INIT',
+        data: [],
+        isLast: false
     }
 };
 
@@ -32,6 +37,32 @@ export default function memo(state, action) {
                 post: {
                     status: { $set: 'FAILURE' },
                     error: { $set: action.error }
+                }
+            });
+        case types.MEMO_LIST:
+            return update(state, {
+                list: {
+                    status: { $set: 'WAITING' }
+                }
+            });
+        case types.MEMO_LIST_SUCCESS:
+            if (action.isInitial) {
+                return update(state, {
+                    list: {
+                        status: { $set: 'SUCCESS' },
+                        data: { $set: action.data },
+                        // 한 페이지에 메모는 6개
+                        // 따라서 메모가 6개 미만이라면 마지막 페이지가 아니다.
+                        isLast: { $set: action.data.length < 6 }
+                    }
+                });
+            }
+
+            return state;
+        case types.MEMO_LIST_FAILURE:
+            return update(state, {
+                list: {
+                    status: { $set: 'FAILURE' }
                 }
             });
         default:
