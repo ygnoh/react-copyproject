d223fc3455039ed62d50ad8bbc7635642aca8c4b
yonggoonoh <nohyg90@gmail.com>
2017-12-05 23:36:26 +0900

    feat: Memo 컴포넌트 내부 작성

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -3,10 +3,30 @@ import React, { Component } from 'react'
 class Memo extends Component {
     render() {
         return (
-            <div>
-                Memo
+            <div className="container memo">
+                <div className="card">
+                    <div className="info">
+                        <a className="username">Writer</a> wrote a log * 1 seconds ago
+                        <div className="option-button">
+                            <a className="dropdown-button" id="dropdown-button-id" data-activates="dropdown-id">
+                                <i className="material-icons icon-button">more_vert</i>
+                            </a>
+                            <ul id="dropdown-id" className="dropdown-content">
+                                <li><a>Edit</a></li>
+                                <li><a>Remove</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="card-content">
+                        Contents
+                    </div>
+                    <div className="footer">
+                        <i className="material-icons log-footer-icon star icon-button">star</i>
+                        <span className="star-count">0</span>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
 
