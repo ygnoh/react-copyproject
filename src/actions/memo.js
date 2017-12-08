843f9de6d688adff9ee8af33c63564ce515082fc
yonggoonoh <nohyg90@gmail.com>
2017-12-09 01:22:33 +0900

    feat memo star action type, action function 구현

--- src/actions/memo.js
+++ src/actions/memo.js
@@ -10,7 +10,10 @@ import {
     MEMO_EDIT_FAILURE,
     MEMO_REMOVE,
     MEMO_REMOVE_SUCCESS,
-    MEMO_REMOVE_FAILURE
+    MEMO_REMOVE_FAILURE,
+    MEMO_STAR,
+    MEMO_STAR_SUCCESS,
+    MEMO_STAR_FAILURE
 } from './ActionTypes';
 import axios from 'axios';
 
@@ -161,6 +164,39 @@ export function memoRemoveSuccess(index) {
 export function memoRemoveFailure(error) {
     return {
         type: MEMO_REMOVE_FAILURE,
+        error
+    };
+}
+
+export function memoStarRequest(id, index) {
+    return (dispatch) => {
+        return axios.post('/api/memo/star/' + id)
+                    .then(response => {
+                        dispatch(memoStarSuccess(index, response.data.memo));
+                    })
+                    .catch(error => {
+                        dispatch(memoStarFailure(error.response.data.code));
+                    });
+    };
+}
+
+export function memoStar() {
+    return {
+        type: MEMO_STAR
+    };
+}
+
+export function memoStarSuccess(index, memo) {
+    return {
+        type: MEMO_STAR_SUCCESS,
+        index,
+        memo
+    };
+}
+
+export function memoStarFailure(error) {
+    return {
+        type: MEMO_STAR_FAILURE,
         error
     };
 }
