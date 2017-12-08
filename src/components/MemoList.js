22412ddcb7b9d6b6aeb8884766d99f8c3d1b3b4f
yonggoonoh <nohyg90@gmail.com>
2017-12-09 01:11:20 +0900

    feat: 삭제 animation 구현

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { Memo } from 'components';
 
 class MemoList extends Component {
     render() {
         const mapToComponents = data => {
             return data.map((memo, i) => {
-                return (<Memo data={memo}
-                                ownership={(memo.writer === this.props.currentUser)}
-                                key={memo._id}
-                                index={i}
-                                onEdit={this.props.onEdit}
-                                onRemove={this.props.onRemove}
+                return (<Memo
+                            data={memo}
+                            ownership={(memo.writer === this.props.currentUser)}
+                            key={memo._id}
+                            index={i}
+                            onEdit={this.props.onEdit}
+                            onRemove={this.props.onRemove}
                         />);
             });
         };
 
         return (
             <div>
-                {mapToComponents(this.props.data)}
+                <ReactCSSTransitionGroup
+                    transitionName="memo"
+                    transitionEnterTimeout={2000}
+                    transitionLeaveTimeout={1000}>
+                    {mapToComponents(this.props.data)}
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
