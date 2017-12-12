a92957da63b4910fc439ea318ea640dd13661c2d
yonggoonoh <nohyg90@gmail.com>
2017-12-12 22:20:14 +0900

    feat: search 관련 action 및 action creator 작성

---
+++ src/actions/search.js
@@ -0,0 +1,36 @@
+import { SEARCH, SEARCH_SUCCESS, SEARCH_FAILURE } from './ActionTypes';
+import axios from 'axios';
+
+export function searchRequest(username) {
+    return (dispatch) => {
+        dispatch(search());
+
+        return axios.get('/search/' + username)
+                    .then(response => {
+                        dispatch(searchSuccess(response.data));
+                    })
+                    .catch(error => {
+                        dispatch(searchFailure(error));
+                    });
+    };
+}
+
+export function search() {
+    return {
+        type: SEARCH
+    };
+}
+
+export function searchSuccess(data) {
+    return {
+        type: SEARCH_SUCCESS,
+        data
+    };
+}
+
+export function searchFailure(error) {
+    return {
+        type: SEARCH_FAILURE,
+        error
+    };
+}
